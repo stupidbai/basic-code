b9cb2ff3843c55c07fde4b2dbcf73be655f2550f
bai <616169394@qq.com>
2020-08-08 18:30:28 +0800

    练习

---
+++ day05-code/src/cn/itcast/day05/demo/demo3/DemoArray.java
@@ -0,0 +1,51 @@
+package cn.itcast.day05.demo.demo3;
+
+public class DemoArray {
+    public static void main(String[] args) {
+        int[] array= {05,25,34,45,55,65,75,8,5,9};
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+        System.out.println("1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        for (int i= 0;i<5;i++) {
+            System.out.println(array[i]);
+        }
+
+        System.out.println("1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        for (int i= 0;i<5;i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//        int lenth(int num){
+//            for (int i= 0;i<num;i++) {
+//                System.out.println(array[i]);
+//            }
+//        }
+
+        int len=array.length;
+        for (int i= 0;i<len;i++) {
+            System.out.println(array[i]);
+        }
+//        System.out.println("1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//        void number(){
+//            int len=array.length;
+//            for (int i= 0;i<len;i++) {
+//                System.out.println(array[i]);
+//            }
+//        }
+        System.out.println("1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        for (int i= 0;i<array.length;i++) {
+            System.out.println(array[i]);
+        }
+
+        System.out.println("1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+
+}
