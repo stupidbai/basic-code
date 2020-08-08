cf1c63863bf751e8b95d9d949031dba036233034
bai <616169394@qq.com>
2020-08-08 16:14:53 +0800

    数组越界

---
+++ day05-code/src/cn/itcast/day05/demo/demo3/Demo01Arrayindex.java
@@ -0,0 +1,13 @@
+package cn.itcast.day05.demo.demo3;
+
+public class Demo01Arrayindex {
+    public static void main(String[] args) {
+        int [] array ={15,25,35};
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+//        System.out.println(array[3]);
+    }
+
+
+}
