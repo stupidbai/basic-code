27acf709d777f75a71be1f622987d8873573f64c
bai <616169394@qq.com>
2020-08-09 13:07:23 +0800

    练习

---
+++ day06-code/src/cn/itcast/day06/Demo01PringtArray.java
@@ -0,0 +1,28 @@
+package cn.itcast.day06;
+
+import java.util.Arrays;
+
+public class Demo01PringtArray {
+
+
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+
+//        System.out.print("[");
+////        for (int i = 0; i < array.length - 1; i++) {
+////            System.out.print(array[i]);
+////            System.out.print(",");
+////        }
+////        System.out.print("]");
+//        for (int i = 0; i <array.length ; i++) {
+//            if (i==array.length-1){
+//                System.out.print(array[i]+"]");
+//            }else {
+//                System.out.print(array[i]+",");
+//            }
+//        }
+
+        System.out.println("*****************************************************************");
+        System.out.println(Arrays.toString(array));
+    }
+}
